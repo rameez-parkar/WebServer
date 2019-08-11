f49ce9397d67d59445b5053e1e81e80221f8d04d
rparkar <rparkar@RENTLAPT0011>
2019-08-11 21:15:30 +0530

    Added leap year checker

--- WebServer.App/Dispatcher.cs
+++ WebServer.App/Dispatcher.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace WebServer.App
 {
@@ -16,13 +18,50 @@ namespace WebServer.App
         {
             _context = context;
             _parser = parser;
-            _file = new StaticFileSystem(_parser);
             _response = _context.Response;
         }
 
         public void BuildHttpResponse()
         {
-            string message = _file.ReadFile();
+            string message = "";
+            if (_parser.Type.ToUpper() == "POST")
+            {
+                try
+                {
+                    string leapYear = "";
+                    JObject jsonObj = JObject.Parse(_parser.Data);
+                    Dictionary<string, string> dictObj = jsonObj.ToObject<Dictionary<string, string>>();
+                    foreach (var item in dictObj)
+                    {
+                        int year = int.Parse(item.Value);
+                        if(year % 400 != 0)
+                        {
+                            if((year % 4 ==0 ) && (year % 100 != 0))
+                            {
+                                leapYear += $"{year} is a leap year.\n";
+                            }
+                            else
+                            {
+                                leapYear += $"{year} is not a leap year.\n";
+                            }
+                        }
+                        else
+                        {
+                            leapYear += $"{year} is a leap year.\n";
+                        }
+                    }
+                    message = leapYear;
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                _file = new StaticFileSystem(_parser);
+                message = _file.ReadFile();
+            }
             buffer = Encoding.UTF8.GetBytes(message);
             _response.ContentLength64 = buffer.Length;
         }
@@ -30,13 +69,21 @@ namespace WebServer.App
         {
             Stream st = _response.OutputStream;
             st.Write(buffer, 0, buffer.Length);
-            if (_file.isFileFound)
+            string type = _parser.RequestData().Item2;
+            if (type.ToUpper() == "GET")
             {
-                _context.Response.StatusCode = (int)HttpStatusCode.OK;
+                if (_file.isFileFound)
+                {
+                    _context.Response.StatusCode = (int)HttpStatusCode.OK;
+                }
+                else
+                {
+                    _context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                }
             }
             else
             {
-                _context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                _context.Response.StatusCode = (int)HttpStatusCode.OK;
             }
             Console.WriteLine("Request served...");
             _context.Response.Close();
--- WebServer.App/HttpRequestParser.cs
+++ WebServer.App/HttpRequestParser.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using System.Web;
 
 namespace WebServer.App
 {
@@ -12,6 +14,9 @@ namespace WebServer.App
         public string Filename { get; set; }
         public string RootDirectory { get; set; }
 
+        public string Data { get; set; }
+        public string Type { get; set; }
+
         public HttpRequestParser(HttpListener listener, ServerConfiguration config, HttpListenerContext context)
         {
             _listener = listener;
@@ -19,11 +24,27 @@ namespace WebServer.App
             _config = config;
         }
 
+        public (string, string) RequestData()
+        {
+            var data = new StreamReader(_context.Request.InputStream, _context.Request.ContentEncoding).ReadToEnd();
+            var requestType = _context.Request.HttpMethod;
+            return (HttpUtility.UrlDecode(data), requestType);
+        }
+
         public void ParseRequest()
         {
             BaseUrl = _context.Request.Url.Scheme + "://" + _context.Request.Url.Authority + "/";
-            Filename = _context.Request.Url.LocalPath;
-            RootDirectory = _config.Mapper.GetValueOrDefault(BaseUrl);
+            Data = RequestData().Item1;
+            Type = RequestData().Item2;
+            if (Type.ToUpper() == "GET")
+            {
+                Filename = _context.Request.Url.LocalPath;
+                RootDirectory = _config.Mapper.GetValueOrDefault(BaseUrl);
+            }
+            else if(Type.ToUpper() == "POST")
+            {
+
+            }
         }
     }
 }
